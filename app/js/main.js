a7d5fd167d096495a85f0e88d7364998500917c1
DmytroSamarasinghe <dima35x@ukr.net>
2020-03-21 20:08:41 +0200

    added menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
-$(function(){
 
+$(function () {
+    function openOffcanvas() {
+        document.getElementById("myOffcanvas").style.width = "175px";
+        document.getElementById("myOffcanvas").style.position = "fixed";
+    }
+    function openNav3() {
+        document.getElementById("myCanvasNav").style.width = "100%";
+        document.getElementById("myCanvasNav").style.opacity = "0.8";
+    }
+    function closeOffcanvas() {
+        document.getElementById("myOffcanvas").style.width = "0%";
+        document.body.style.backgroundColor = "white";
+        document.getElementById("myCanvasNav").style.width = "0%";
+        document.getElementById("myCanvasNav").style.opacity = "0";
+    }
 });
